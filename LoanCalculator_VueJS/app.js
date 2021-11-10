2dead96100c72f6fbcfcdb9861485764a3842a48
TrishiaHamo-ay <89622810+TrishiaHamo-ay@users.noreply.github.com>
2021-11-10 19:09:24 +0800

    Update app.js

--- LoanCalculator_VueJS/app.js
+++ LoanCalculator_VueJS/app.js
@@ -3,6 +3,8 @@ import { createApp } from 'vue';
 import App from './components/App.vue';
 createApp(App).mount("#example");
 
+
+//CHATROOM
 require('./bootstrap');
 
 const message_el = document.getElementById("messages");
